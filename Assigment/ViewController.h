39e9625ad9d285a177ebde9557ae068dc69c7dce
Redblink Devs <rbpcadmin@macintoish.local>
2016-04-29 19:56:06 +0530

    Initial Commit

---
+++ Assigment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assigment
+//
+//  Created by rbpcadmin on 4/29/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Assigment/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Assigment
+//
+//  Created by rbpcadmin on 4/29/16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
